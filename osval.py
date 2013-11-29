4948eaa1ae45d3a916bd8b12263cfc018d207033
bjl7 <bjl7@eddore.cs.st-andrews.ac.uk>
2013-11-29 00:15:47 +0000

    added command for classifying

--- osval.py
+++ osval.py
@@ -1,10 +1,11 @@
 from twisted.words.protocols import irc
 from twisted.internet import protocol, reactor
-from counter import Counter
+from collections import Counter
 import random
 import collections
 import os, re
 import train
+#from pprint import pprint
 
 class MarkovChain(object):
     def __init__(self):
@@ -22,19 +23,20 @@ class MarkovChain(object):
         self.chain[tuple(buf)].append('\n')
 
     def generate(self, ngram, seed=None):
+        random.seed(os.urandom(5))
         if seed:
             buf = seed.split()[:ngram]
             #message = [random.choice(self.chain[random.choice(self.chain.keys())])]
             if len (seed.split()) > ngram:
-                message = buf[:]
+                message = buf[:ngram]
             else:
-                message = []
-                for i in xrange(ngram):
-                    message.append(random.choice(
+                for i in xrange(ngram-len(seed.split())):
+                    buf.append(random.choice(
                         self.chain[random.choice(self.chain.keys())]))
         else:
             buf = list(random.choice(self.chain.keys()))
-            message = buf[:]
+        #pprint(self.chain)
+        message = buf[:]
 
         for i in xrange(1000):
             try:
@@ -56,8 +58,18 @@ class EnvironmentStuff(object):
         self.signouts = Counter()
         self.kicks = Counter()
         self.markov = MarkovChain()
-        self.commands = { "pos":lambda x: x*3}
-        
+        self.commands = {"train": train_it, "clas": classify, "classify": classify} 
+
+def classify(msg):
+    print(msg)
+    cat = train.classify(msg)
+    if not cat:
+        return "I have no thoughts on the matter."
+    return cat.name
+
+def train_it(fuck_off=None):
+    train.do_training()
+    return "trained!"
 
 class Osvalbot(irc.IRCClient):
     def _get_nickname(self):
@@ -66,12 +78,13 @@ class Osvalbot(irc.IRCClient):
 
     def signedOn(self):
         print("adding babble")
-        if os.path.exists("babble.txt"):
-            with open('babble.txt', 'r') as f:
+        if os.path.exists("babbling.txt"):
+            with open('babbling.txt', 'r') as f:
                 for line in f:
                     #it's like it's java! #ohgod
-                    self.factory.environment.markov.add(line)
-        self.join(self.factory.channel)
+                    self.factory.environment.markov.add(line, 2)
+        for channel in self.factory.channels:
+            self.join(channel)
         print("Signed on as "+self.nickname)
 
     def joined(self, channel):
@@ -82,34 +95,54 @@ class Osvalbot(irc.IRCClient):
 
     def privmsg(self, user, channel, msg):
         seeded = False
+        commanding = False
         if not user:
             return
         if self.nickname in msg:
             msg = re.compile(self.nickname + "[:,]* ?", re.I).sub('',msg)
-            if msg[0] == '!':
-                self.factory.environment.commands[msg[1:]](msg)
-            elif msg[0] == '$':
-                msg = msg[1:]
-                seeded = True
-            prefix = "%s: " % (user.split('!', 1)[0],)
+            if msg:
+                print msg
+                if msg[0] == '!':
+                    commanding = True
+                    commandmatch = re.match("!(\w*) ", msg)
+                    if commandmatch:
+                        print "firstmatch type"
+                        command = commandmatch.group(1)
+                    else:
+                        commandmatch = re.match("!(.*)", msg)
+                        command = commandmatch.group(1) #too many ifs
+                    commandmatch = re.match("!"+command+" (.*)", msg)
+                    if commandmatch:
+                        command_args = commandmatch.group(1)
+                    else:
+                        command_args = None
+                    print("command "+command)
+                    if command_args :print(", args \""+command_args+"\"")
+                elif msg[0] == '$':
+                    msg = msg[1:]
+                    seeded = True
+                prefix = "%s: " % (user.split('!', 1)[0],)
         else:
             prefix =''
         
-        self.factory.environment.markov.add(msg, 2, write_to_file=True)
         if prefix or random.random() <= self.factory.chattiness:
             if seeded:
                 sentence = self.factory.environment.markov.generate(2, seed=msg)
+            elif commanding:
+                sentence = str(self.factory.environment.commands[command](command_args))
             else:
                 sentence = self.factory.environment.markov.generate(2)
             if sentence:
-                self.msg(self.factory.channel, prefix+sentence)
+                self.msg(channel, prefix+sentence.lstrip())
+        else:
+            self.factory.environment.markov.add(msg, 2, write_to_file=True)
 
 
 class OsvalbotFactory(protocol.ClientFactory):
     protocol = Osvalbot
 
-    def __init__(self, channel="#stacs", nickname="osvalbot", chattiness=0.01):
-        self.channel = channel
+    def __init__(self, channels=["#stacs", "#stacs2"], nickname="osvalbot", chattiness=0.01):
+        self.channels = channels
         self.nickname = nickname
         self.environment = EnvironmentStuff()
         self.chattiness = chattiness
